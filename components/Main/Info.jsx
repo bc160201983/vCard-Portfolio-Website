271dcdb2c3b5bb13de7c33d5f1a7b4230bd43906
bilal afzaal <softnacuk@gmail.com>
2022-03-11 18:50:07 +0500

    image tag fixed

--- components/Main/Info.jsx
+++ components/Main/Info.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "next/Image";
 
 const Info = ({ title, img, desc }) => {
   return (
     <div>
       <div className="main-info flex justify-between rounded-[16px] mb-[30px] px-[30px] pt-[30px] pb-[20px] box-info-shadow box--s2">
         <div className="icon-info mr-4">
-          <img className="w-10 h-10 max-w-full" src={img} alt="" />
+          <Image width={40} height={40} src={img} alt="" />
         </div>
         <div className="info-details">
           <div className="text-[18px] text-[#FCFCFC] pb-2 font-[600]">
