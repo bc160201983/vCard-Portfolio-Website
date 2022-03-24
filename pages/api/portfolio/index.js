18f3b92c13264868facb73bd7677ccb405d6f905
bilal afzaal <softnacuk@gmail.com>
2022-03-24 23:28:22 +0500

    cors fixed

--- pages/api/portfolio/index.js
+++ pages/api/portfolio/index.js
@@ -1,9 +1,20 @@
+import Cors from "cors";
+import initMiddleware from "../../../lib/init-middleware";
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
 export default async function handler(req, res) {
   //   // Check for secret to confirm this is a valid request
   //   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
   //     return res.status(401).json({ message: "Invalid token" });
   //   }
-
+  await cors(req, res);
   if (req.method === "GET") {
     try {
       await res.unstable_revalidate("/portfolio");
