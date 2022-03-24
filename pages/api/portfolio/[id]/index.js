a5ab43af7bbd57b4cd0d76490b61ceaa9dd18b2a
bilal afzaal <softnacuk@gmail.com>
2022-03-25 00:53:17 +0500

    update

--- pages/api/portfolio/[id]/index.js
+++ pages/api/portfolio/[id]/index.js
@@ -1,5 +1,5 @@
 import Cors from "cors";
-import initMiddleware from "../../../lib/init-middleware";
+import initMiddleware from "../../../../lib/init-middleware";
 // Initialize the cors middleware
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
@@ -10,7 +10,6 @@ const cors = initMiddleware(
 );
 
 export default async function handler(req, res) {
-  console.log(req.query);
   //   // Check for secret to confirm this is a valid request
   //   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
   //     return res.status(401).json({ message: "Invalid token" });
@@ -18,8 +17,8 @@ export default async function handler(req, res) {
   await cors(req, res);
   if (req.method === "GET") {
     try {
-      await res.unstable_revalidate("/portfolio");
-      return res.json({ revalidated: true, msg: "get Request" });
+      await res.unstable_revalidate("/portfolio/" + req.query.id);
+      return res.json({ revalidated: true, msg: `get Request${req.query.id}` });
     } catch (err) {
       // If there was an error, Next.js will continue
       // to show the last successfully generated page
